49c01e0682c2c9935e4850e9a293dc253796651b
Calista Nguyen <calistanguyen@Calistas-MacBook-Pro.local>
2022-04-20 20:02:22 -0600

    lollll

--- twurd/src/App.js
+++ twurd/src/App.js
@@ -1,14 +1,26 @@
 import {Header} from './components/header'
 import {Map} from './components/map'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {DataSection} from './components/dataSection'
 function App() {
   const [state, setState] = useState("");
-
+  const [stateSelected, setStateSelected] = useState(false)
+  const [json, setJson] = useState({});
+  
   const stateChangeListener = (name) =>{
-    setState(name)
+    fetch("/frequencies/" + name).then(
+      res => res.json()
+    ).then(
+      data => {
+        console.log("HELLO")
+        console.log(data)
+        setJson(data)
+        setStateSelected(true)
+        setState(name)
+        
+    } 
+    )
   }
-
 
   return (
     <div className= "row">
@@ -17,12 +29,20 @@ function App() {
         <Map stateChangeListener={stateChangeListener}/>
       </div>
       <div className="colInfo">
-          <div className="infoSection">
-            <div className = "stateHeader">Top words for state: {state}</div>
+      {
+        !stateSelected && <div>select a state</div>
+      }
+          {stateSelected &&
+            <div className="infoSection">
+            {json && <div className = "stateHeader">Top words for state: {json.state}</div>}
             <hr/>
-          <DataSection Primarycolor={'0'} progress = {70} word = "hi" numTweeted={70}/>
-          <DataSection Primarycolor={'1'} progress = {30} word = "bye" numTweeted={30}/>
-          <DataSection Primarycolor={'2'} progress = {50} word = "hola" numTweeted={50}/>
+          <DataSection Primarycolor={'0'} progress = {70} word = {json.words[0]} numTweeted={70}/>
+          <DataSection Primarycolor={'1'} progress = {30} word = {json.words[1] }numTweeted={30}/>
+          <DataSection Primarycolor={'2'} progress = {50} word = {json.words[2]} numTweeted={50}/>
+          </div>}
+          <div>
+          </div>
+          <div>
           </div>
       </div>
     </div>
